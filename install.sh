a6fa6e57ea375e791f0eeba582902abe5f9eb796
deut-erium <himanshuthesheoran@gmail.com>
2021-07-11 20:32:37 +0530

    pypy and basic tools

--- install.sh
+++ install.sh
@@ -7,8 +7,25 @@ apt-get -y update && apt-get -y upgrade && \
       p7zip \
       vim
 
-add-apt-repository ppa:deadsnakes/ppa
+add-apt-repository -y ppa:deadsnakes/ppa ppa:pypy/ppa
 apt-get install -y python3.9 python3-pip
+
+apt-get install -y libgmp3-dev libmpfr-dev libmpc-dev
+apt-get install -y \
+    pypy3 \
+    gdb \
+    # sagemath-common \
+    # sagemath \
+    nmap \
+    binwalk \
+    nasm \
+    fasm \
+    radare2 \
+    exif
+
+
+
+
 sudo apt-get install -y docker \
       docker-compose \
       tmux \
@@ -17,7 +34,6 @@ sudo apt-get install -y docker \
       maven \
       gradle 
 
-apt-get install -y libgmp3-dev libmpfr-dev libmpc-dev
 
 pip3 install tqdm \
       z3-solver \
@@ -36,6 +52,15 @@ pip3 install tqdm \
       scipy \
       sklearn \
       autopep8
+
+pypy3 -m pip install \
+    tqdm \
+    z3-solver \
+    sympy \
+    pycryptodome \
+    numpy \
+    pillow \
+
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # docker pull hyperreality/cryptohack
